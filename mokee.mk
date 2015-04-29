24583641e757cd045930dfebe5d3eb129692dc83
martincz <martincz@mokeedev.com>
2015-04-30 06:36:41 +0800

    mokee bringup

---
+++ mokee.mk
@@ -0,0 +1,7 @@
+$(call inherit-product, device/samsung/d2-common/d2-common.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
+
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
